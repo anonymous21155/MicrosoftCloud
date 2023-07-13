09694b045b9365a5a25b14ea5ac5f5c4345d5ef7
anonymous21155 <gokul.dev@10xds.com>
2023-07-13 10:44:58 +0530

    new

--- samples/acs-to-teams-meeting/client/react/src/App.tsx
+++ samples/acs-to-teams-meeting/client/react/src/App.tsx
@@ -1,14 +1,23 @@
 import { AzureCommunicationTokenCredential, CommunicationUserIdentifier } from '@azure/communication-common';
 import {  
   CallWithChatComposite,
-  useAzureCommunicationCallWithChatAdapter, 
-  fromFlatCommunicationIdentifier, 
+  useAzureCommunicationCallWithChatAdapter,
+  COMPOSITE_LOCALE_EN_US,
+  fromFlatCommunicationIdentifier,
+  darkTheme, 
+  CallWithChatAdapter,
+  CallWithChatAdapterState
 } from '@azure/communication-react';
-import React, { useState, useMemo, useEffect } from 'react';
+import React, { useState, useMemo, useEffect, useCallback } from 'react';
+import MobileDetect from 'mobile-detect';
 import './App.css';
 
 const App = () => { 
   const displayName = 'Guest';
+  const logoUrl = 'https://th.bing.com/th/id/OIP.65eMKYCyemDCcSplj51IEgAAAA?pid=ImgDet&rs=1';
+  const locale = COMPOSITE_LOCALE_EN_US;
+  const waitingTitle = 'Thankyou for contacting 10xDS Customer Service';
+  const waitingSubtitle ='Service agent might be engaged with other customers, please be patient';
   const [endpointUrl, setEndpointUrl] = useState<string>('');
   const [threadId, setThreadId] = useState<string>('');
   const [userId, setUserId] = useState<string>('');
@@ -21,6 +30,12 @@ const App = () => {
     }
     return;
     }, [token]);
+  
+    const afterAdapterCreate = useCallback(async (adapter: CallWithChatAdapter): Promise<CallWithChatAdapter> => {
+      adapter.joinCall(true); 
+      return new Promise((resolve, reject) => resolve(adapter));
+    }, []);
+
 
   const callWithChatAdapterArgs = useMemo(() => {
     if (userId && endpointUrl && credential && displayName && threadId && teamsMeetingLink) {
@@ -36,7 +51,7 @@ const App = () => {
     return {};
   }, [userId, endpointUrl, credential, displayName, threadId, teamsMeetingLink]);
 
-  const callWithChatAdapter = useAzureCommunicationCallWithChatAdapter(callWithChatAdapterArgs);
+  const callWithChatAdapter = useAzureCommunicationCallWithChatAdapter(callWithChatAdapterArgs, afterAdapterCreate);
 
   
 
@@ -63,13 +78,34 @@ const App = () => {
 
 }, []);
   if (callWithChatAdapter) {
+    const logo = logoUrl ? <img src={logoUrl} /> : <></>;
+    const formFactorValue = new MobileDetect(window.navigator.userAgent).mobile() ? 'mobile' : 'desktop';
     return (
       <div>
-        <h1>Contact Customer Service</h1>
+        
         <div className="wrapper">
           <CallWithChatComposite
             adapter={callWithChatAdapter}
-            formFactor="mobile"
+            formFactor={formFactorValue}
+            fluentTheme={darkTheme}
+            locale={{
+              component: locale.component,
+              strings: {
+                chat: locale.strings.chat,
+                call: {
+                  ...locale.strings.call,
+                  lobbyScreenConnectingToCallTitle: waitingTitle,
+                  lobbyScreenConnectingToCallMoreDetails: waitingSubtitle,
+                  lobbyScreenWaitingToBeAdmittedTitle: waitingTitle,
+                  lobbyScreenWaitingToBeAdmittedMoreDetails: waitingSubtitle
+                },
+                callWithChat: locale.strings.callWithChat
+              }
+            }}
+            icons={{
+              LobbyScreenWaitingToBeAdmitted: logo,
+              LobbyScreenConnectingToCall: logo
+            }}
           />
           
         </div>
